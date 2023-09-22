2927f5e6371d754f687aea937f272f2046a313fc
Vladislav Ananev <vananyev@hystax.com>
2023-09-22 23:59:56 +0400

    OS-6707: Build.sh improvements

--- build.sh
+++ build.sh
@@ -1,45 +1,63 @@
 #!/usr/bin/env bash
 
+# ./build.sh [component] [tag] [-r registry] [-u username] [-p password]
+# leave registry empty if default registry [docker.io] used
+
 set -e
 
-INPUT_TAG=$2
+# Initialize default values
+REGISTRY=""
+LOGIN=""
+PASSWORD=""
+COMPONENT=""
+INPUT_TAG=""
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r) REGISTRY="$2"; shift ;;
+        -u) LOGIN="$2"; shift ;;
+        -p) PASSWORD="$2"; shift ;;
+        *)
+            # Check if COMPONENT is empty
+            if [[ -z "$COMPONENT" ]]; then
+                COMPONENT="$1"
+            else
+                # If COMPONENT is already set, then set BUILD_TAG
+                INPUT_TAG="$1"
+            fi
+            ;;
+    esac
+    shift
+done
+
+
 BUILD_TAG=${INPUT_TAG:-'build'}
-REGISTRY=$3
-LOGIN=$4
-PASS=$5
-
 FIND_CMD="find . -mindepth 2 -maxdepth 3 -print | grep Dockerfile | grep -vE '(test|.j2)'"
+FIND_CMD="${FIND_CMD} | grep $COMPONENT/"
 
-case $# in
-    0)
-    ;;
-    1|2|3|4|5)
-    FIND_CMD="${FIND_CMD} | grep $1/"
-    ;;
-    *)
-    echo "invalid arguments number, use ./build.sh [component] [build tag]"
-    exit 1
-esac
-
-if [ ! -z "$LOGIN" ]; then
+if [[ -n "${LOGIN}" && -n "${PASSWORD}" ]]; then
   echo "docker login"
-  docker login $REGISTRY -u "${LOGIN}" -p "${PASS}"
-  fi
+  docker login ${REGISTRY} -u "${LOGIN}" -p "${PASSWORD}"
+fi
+
+push_image () {
+   echo "Pushing $2"
+    if [ -z $1]; then
+      docker tag "$2:$3" "hystax/$2:$3"
+      docker push "hystax/$2:$3"
+    else
+      docker tag "$2:$3" "$1/$2:$3"
+      docker push "$1/$2:$3"
+    fi
+}
 
 for DOCKERFILE in $(eval ${FIND_CMD} | xargs)
 do
     COMPONENT=$(echo "${DOCKERFILE}" | awk -F '/' '{print $(NF-1)}')
     echo "Building image for ${COMPONENT}, build tag: ${BUILD_TAG}"
     docker build -t ${COMPONENT}:${BUILD_TAG} -f ${DOCKERFILE} .
-    if [ ! -z "$REGISTRY" ]; then
-      echo "Pushing ${COMPONENT} to ${REGISTRY}"
-
-      if [ $REGISTRY == "dr-dev.hystax.com" ]; then
-        docker tag "${COMPONENT}:${BUILD_TAG}" "${REGISTRY}/${COMPONENT}:${BUILD_TAG}"
-        docker push "${REGISTRY}/${COMPONENT}:${BUILD_TAG}"
-      else
-        docker tag "${COMPONENT}:${BUILD_TAG}" "hystax/${COMPONENT}:${BUILD_TAG}"
-        docker push "hystax/${COMPONENT}:${BUILD_TAG}"
-      fi
+    if [[ -n "${LOGIN}" && -n "${PASSWORD}" ]]; then
+      push_image $REGISTRY $COMPONENT $BUILD_TAG
     fi
 done
